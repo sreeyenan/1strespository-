ad1b6e7feb91e85702236ed9e47b5cde9288d8e8
sreeyenan <sreeyenan@gmail.com>
2019-10-04 10:23:27 +0530

    initial commit

---
+++ SalesTools2019/src/com/ust/test/Main.java
@@ -0,0 +1,20 @@
+package com.ust.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData objSalesData =new SalesData();
+		
+		System.out.println("Hello guys git ripo");
+		displayGreetings();
+		objSalesData.display();
+
+	}
+
+	private static void displayGreetings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ SalesTools2019/src/com/ust/test/SalesData.java
@@ -0,0 +1,8 @@
+package com.ust.test;
+
+public class SalesData {
+	void display(){
+		System.out.println("Sales repport");
+	}
+
+}
